3bf01971b9c2622221fa549d6aec0d264b807b73
jorgegrajalesu <jegosystem@gmail.com>
2023-07-25 18:52:14 -0500

    250723

--- js/app.js
+++ js/app.js
@@ -3,3 +3,5 @@ let rate1 = document.querySelector('.rate1');
 let rate2 = document.querySelector('.rate2');
 let resultBtn = document.querySelector('.result');
 let selects = document.querySelectorAll('.options select');
+let sel1 = selects[0];
+let sel2 = selects[1];
