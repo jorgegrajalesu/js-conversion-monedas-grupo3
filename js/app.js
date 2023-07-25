6da78cc39d8ca7d6d4e2ed9303b65e72c84939ea
jorgegrajalesu <jegosystem@gmail.com>
2023-07-25 18:50:10 -0500

    250723

---
+++ js/app.js
@@ -0,0 +1,5 @@
+// declarar variables para capturar el dato 
+let rate1 = document.querySelector('.rate1');
+let rate2 = document.querySelector('.rate2');
+let resultBtn = document.querySelector('.result');
+let selects = document.querySelectorAll('.options select');
